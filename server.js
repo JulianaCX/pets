7560e2cce54d2e47f6553bc80ecab508174137b9
JulianaCX <41385197+JulianaCX@users.noreply.github.com>
2019-05-21 14:59:57 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -12,82 +12,113 @@ app.use(express.static(__dirname));
 const http = require('http');
 
 
-/*http.createServer(function (req, res) {
-  // server code
-
-  console.log(`${req.method} ${req.url}`);
-}).listen(8080);
-*/
 app.listen(8080, function(){
-    console.log('it works')
+  console.log('it works')
 })
-  app.get('/', urlencodedParser, function (req, res) {
-    res.sendFile(__dirname + '/index.htm');
-  })
+app.all('/', urlencodedParser, function (req, res) {
+  res.sendFile(__dirname + '/index.htm');
+})
 
+//register
+app.post('/register', urlencodedParser, function (req, res) {
+  console.log('register request')
+  var eaw = true
+  email = req.body.email1
+   
+  let db = new sqlite3.Database('accounts.db');
+  db.all(`SELECT id, salt, password FROM accounts WHERE ?="username"`, [email], (err, rows) => {
+    rows.forEach((rows)=>{
+      console.log(rows)
+      eaw = false
+      console.log(eaw + 'forEach')
+    })
 
-  app.post('/register', urlencodedParser, function (req, res) {
-    console.log('register request')
-     email = req.body.email1
-     password = req.body.pword
-     let db = new sqlite3.Database('accounts.db');
+    if(eaw){
+      console.log(eaw)
+      console.log('entering into database')
+      var saltss = crypto.randomBytes(8).toString('hex').slice(0,16)
+      password = req.body.pword
+      password = hashsalt(password, saltss)
+      db.run(`INSERT INTO accounts (password,username,salt) VALUES (?, ?, ?)`, [password, email, saltss])
+//
+      db.all(`SELECT id, salt, password FROM accounts WHERE ?="username"`, [email], (err, rows) => {
+        rows.forEach((row)=>{
+          res.cookie("TheID", row.id)
+        })
 
-     var saltss = crypto.randomBytes(8).toString('hex').slice(0,16)
-
-     password = hashsalt(password, saltss)
-
-     db.run(`INSERT INTO accounts (password,username,salt) VALUES (?, ?, ?)`, [password, email, saltss])
-
-     response = {
+        response = {
         email:req.body.email1,
         password:req.body.pword,
         salts:saltss
      }
-     console.log(response)
-    res.sendFile(__dirname + '/index.htm');
 
-  })
-  app.post('/homepage', urlencodedParser, function (req, res) {
-     email = req.body.email2
-     password = req.body.loginPword
-     let db = new sqlite3.Database('accounts.db');
 
-     db.all(`SELECT id, salt, password FROM accounts WHERE ?="username"`, [email], (err, rows) => {
-        rows.forEach((row)=>{
-           if(hashsalt(password, row.salt)== row.password){
-              console.log('authenticated')
-              req.session.userId = row.id
-              
-              res.redirect('/main');
-            }
-           else{
-            res.send('/index.html')
-              res.sendFile(__dirname + '/index.htm')
-           }
-        })
-        
-     })
+    
+    console.log(response)
+    res.redirect('/longtermgoals');
+   
 
-  })
-  app.get('/test', urlencodedParser, function (req, res) {
-    console.log(req.session.userId)
+      })
+//
+
+
+    }
+
+    else{
+      res.redirect('/')
+    }
   })
 
-
-function hashsalt(password, salts){
-    var hash = crypto.createHmac('sha512', salts)
-    hash.update(password);
-    return hash.digest('hex')
-}
-function hashsalt(password, salts){
-    var hash = crypto.createHmac('sha512', salts)
-    hash.update(password);
-    return hash.digest('hex')
-}
-
-app.get('/homepage', urlencodedParser, function (req, res) {
-    res.sendFile(__dirname + '/home.html')
 })
 
 
+
+  //login
+app.post('/homepage', urlencodedParser, function (req, res) {
+  helper = '/'
+   email = req.body.email2
+   passwrd = req.body.loginPword
+
+   let db = new sqlite3.Database('accounts.db');
+
+   console.log(passwrd)
+
+   db.all(`SELECT id, salt, password FROM accounts WHERE ?="username"`, [email], (err, rows) => {
+      rows.forEach((row)=>{
+         if(hashsalt(passwrd, row.salt)== row.password){
+            console.log('authenticated')
+            res.cookie("TheID" , row.id)
+            helper = '/home'
+          }
+      })
+      res.redirect(helper)
+   })
+   
+})
+
+
+
+
+app.all('/addgoals', urlencodedParser, function (req, res) {
+  
+
+})
+
+
+function hashsalt(password, salts){
+    var hash = crypto.createHmac('sha512', salts)
+    hash.update(password);
+    return hash.digest('hex')
+}
+
+app.get('/home', urlencodedParser, function (req, res) {
+    res.sendFile(__dirname + '/home.html')
+})
+
+app.get('/longtermgoals', urlencodedParser, function (req, res) {
+    res.sendFile(__dirname + '/longtermgoals.htm')
+})
+app.get('/select_animal', urlencodedParser, function (req, res) {
+    res.sendFile(__dirname + '/intro_animal.htm')
+})
 
